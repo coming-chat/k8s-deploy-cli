05fdad0952735cda16a6084d3a096d4439ba3f47
吴wbh1328551759 <1328551759@qq.com>
2021-09-23 18:28:55 +0800

    update nginx data, add gzip

--- lib/handleNginxWrite.ts
+++ lib/handleNginxWrite.ts
@@ -9,7 +9,25 @@ export const nginxData = 'server {\n' +
   '\n' +
   '    root /usr/share/nginx/html;\n' +
   '    index index.html;\n' +
+  '    gzip on;\n' +
+  '    gzip_disable "msie6";\n' +
   '\n' +
+  '    gzip_comp_level 6;\n' +
+  '    gzip_min_length 1100;\n' +
+  '    gzip_buffers 16 8k;\n' +
+  '    gzip_proxied any;\n' +
+  '    gzip_types\n' +
+  '            text/plain\n' +
+  '            text/css\n' +
+  '            text/js\n' +
+  '            text/xml\n' +
+  '            text/javascript\n' +
+  '            application/javascript\n' +
+  '            application/x-javascript\n' +
+  '            application/json\n' +
+  '            application/xml\n' +
+  '            application/rss+xml\n' +
+  '            image/svg+xml/javascript;\n' +
   '    location / {\n' +
   '        try_files $$uri /index.html;\n' +
   '    }\n' +
